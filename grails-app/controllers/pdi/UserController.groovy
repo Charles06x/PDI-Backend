9b4ab4cbab49cffe50f24ee6713ba3287f3516cc
Charles <chad9591@gmail.com>
2019-05-10 00:33:34 -0500

    Login method in initial state.

--- grails-app/controllers/pdi/UserController.groovy
+++ grails-app/controllers/pdi/UserController.groovy
@@ -4,6 +4,9 @@ package pdi
 import grails.rest.*
 import grails.converters.*
 
+import grails.plugins.rest.client.RestBuilder
+import grails.plugins.rest.client.RestResponse
+
 class UserController extends RestfulController {
     static responseFormats = ['json', 'xml']
     UserController() {
@@ -11,6 +14,16 @@ class UserController extends RestfulController {
     }
 
     def login(){
-        render (params.user + ' & ' + params.pwd)
+
+        RestBuilder req = new RestBuilder()
+        RestResponse res = req.get("http://savio.utbvirtual.edu.co/login/token.php?username="+params.user+"&password="+params.pwd+"&service=moodle_mobile_app")
+        if(res.statusCode.value == 200 && res.json.token) {
+            def query = User.where { 
+                code ==~ "%${params.user}%"           
+            }
+            respond query
+        }
+        render "Not working xD Dx"
+        //"http://savio.utbvirtual.edu.co/login/token.php?username="+params.user+"&password="+params.pwd+"&service=moodle_mobile_app"
     }
 }
--- grails-app/init/pdi/BootStrap.groovy
+++ grails-app/init/pdi/BootStrap.groovy
@@ -3,6 +3,7 @@ package pdi
 class BootStrap {
 
     def init = { servletContext ->
+        def u = new User(code: 't00039395', name: 'charles').save()
     }
     def destroy = {
     }
