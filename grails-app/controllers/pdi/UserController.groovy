a5ab8c961c18106e8847223eaf6b60eac6147263
Charles <chad9591@gmail.com>
2019-05-11 22:58:55 -0500

    Login function improved, returns user + token

--- grails-app/controllers/pdi/UserController.groovy
+++ grails-app/controllers/pdi/UserController.groovy
@@ -7,6 +7,8 @@ import grails.converters.*
 import grails.plugins.rest.client.RestBuilder
 import grails.plugins.rest.client.RestResponse
 
+import groovy.json.JsonSlurper
+
 class UserController extends RestfulController {
     static responseFormats = ['json', 'xml']
     UserController() {
@@ -18,12 +20,14 @@ class UserController extends RestfulController {
         RestBuilder req = new RestBuilder()
         RestResponse res = req.get("http://savio.utbvirtual.edu.co/login/token.php?username="+params.user+"&password="+params.pwd+"&service=moodle_mobile_app")
         if(res.statusCode.value == 200 && res.json.token) {
+            def jsonSlurper = new JsonSlurper()
             def query = User.where { 
                 code ==~ "%${params.user}%"           
             }
-            respond query
+            def resp = query + res.json.token
+            respond resp
         }
-        render "Not working xD Dx"
+        render res.json
         //"http://savio.utbvirtual.edu.co/login/token.php?username="+params.user+"&password="+params.pwd+"&service=moodle_mobile_app"
     }
 }
--- grails-app/init/pdi/BootStrap.groovy
+++ grails-app/init/pdi/BootStrap.groovy
@@ -3,7 +3,7 @@ package pdi
 class BootStrap {
 
     def init = { servletContext ->
-        def u = new User(code: 't00039395', name: 'charles').save()
+        def u = new User(code: 't00039395', rol: 1, name: 'charles').save(flush: true)
     }
     def destroy = {
     }
